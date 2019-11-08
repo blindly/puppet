ba552df525565681e66d37340daa3ba7fae23f5b
Dev <dev@borke.us>
2019-11-08 08:25:51 -0500

    initial import - vhosts created

---
+++ website/vhosts.pp
@@ -0,0 +1,10 @@
+class {'apache': }
+
+apache::vhost { 'vhost.155.138.196.253.xip.io':
+  port    => '80',
+  docroot => '/var/www/vhost',
+}
+
+file {'/var/www/vhost/index.html': 
+  content => "It works";
+}
