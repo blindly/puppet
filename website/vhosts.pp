5f7bb28cbebe008e08a33989230e77fb0f5fd235
Dev <dev@borke.us>
2019-11-08 08:37:46 -0500

    php installation

--- website/vhosts.pp
+++ website/vhosts.pp
@@ -1,5 +1,19 @@
 class {'apache': }
 
+class { '::php::globals':
+  php_version => '7.0',
+  config_root => '/etc/php/7.0',
+}->
+class { '::php':
+  ensure       => latest,
+  manage_repos => true,
+  fpm          => true,
+  dev          => true,
+  composer     => true,
+  pear         => true,
+  phpunit      => false,
+}
+
 apache::vhost { 'vhost.155.138.196.253.xip.io':
   port    => '80',
   docroot => '/var/www/vhost',
