9ce6bdef7374397d7fd94e236973372f73cd2e8f
Dev <dev@borke.us>
2019-11-08 08:29:42 -0500

    adding additional vhosts with the same docroot

--- website/vhosts.pp
+++ website/vhosts.pp
@@ -5,6 +5,17 @@ apache::vhost { 'vhost.155.138.196.253.xip.io':
   docroot => '/var/www/vhost',
 }
 
+apache::vhost { 'vhost1.155.138.196.253.xip.io':
+  port    => '80',
+  docroot => '/var/www/vhost',
+}
+
+apache::vhost { 'vhost2.155.138.196.253.xip.io':
+  port    => '80',
+  docroot => '/var/www/vhost',
+}
+
+
 file {'/var/www/vhost/index.html': 
   content => "It works";
 }
